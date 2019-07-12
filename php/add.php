8a06dab55f22ebb911aef97db378d38360482186
sanya612760 <600112417020@bru.ac.th>
2019-07-12 15:36:07 +0700

    init project

---
+++ php/add.php
@@ -0,0 +1,20 @@
+<?php
+echo $_POST['user'];
+
+echo "Hello world !!!";
+
+$name = "sanya";
+echo "Mr." . $name;
+echo "<br>";
+
+
+echo sum(5,3);
+
+
+function sum($A,$B){
+    return $A + $B;
+}
+?>
+
+//short term use one line noly.
+<?='My name is benz' ?>
